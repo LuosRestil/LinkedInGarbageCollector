841070b444a15601b80be58e88b0c6d244a3a188
Luos Restil <luosrestil@luoss-mbp.lan>
2023-09-18 14:41:16 -0400

    update contentScript logic to more accurately filter out unwanted posts

--- firefox/contentScript.js
+++ firefox/contentScript.js
@@ -1,28 +1,34 @@
+const nonoWordList = [
+  'promoted',
+  'likes this',
+  'loves this',
+  'commented on this',
+  'finds this funny',
+  'celebrates this',
+  'supports this',
+  'finds this insightful',
+];
+const nonoWords = new Set(nonoWordList);
+
+const nonoRootSelectorList = [
+  'div.update-components-header',
+  'div.update-components-actor',
+];
+
 function filterUnwantedPosts() {
-  // nono selectors
-  const nonoDivSelector = 'div.update-components-header'
-  const nonoSpanSelector = `div > ${nonoDivSelector} > div.update-components-header__text-wrapper > span.update-components-header__text-view > div.update-components-text-view > span > span`;
-  const nonos = [" likes this", " loves this", " commented on this", " finds this funny", " celebrates this", " supports this", " finds this insightful"];
-  const promotedText = "Promoted";
-  // promo selectors
-  const promoDivSelector = 'div.update-components-actor';
-  const promoSelectorBase = `div > ${promoDivSelector} > a > div.update-components-actor__meta`;
-  const promoSelector1 = `${promoSelectorBase} > span.update-components-actor__sub-description > div.update-components-text-view > span`;
-  const promoSelector2 = `${promoSelectorBase} > span.update-components-actor__description > span`;
-  
-  // selection
-  const nonoSpans = Array.from(document.querySelectorAll(nonoSpanSelector));
-  const nonoDivs = nonoSpans.filter(span => nonos.includes(span.textContent))
-                            .map(span => span.closest(nonoDivSelector).parentNode);
-  const promoSpans1 = Array.from(document.querySelectorAll(promoSelector1));
-  const promoSpans2 = Array.from(document.querySelectorAll(promoSelector2));
-  const promoSpans = [...promoSpans1, ...promoSpans2];
-  const promoDivs = promoSpans.filter(span => span.textContent === promotedText)
-                              .map(span => span.closest(promoDivSelector).parentNode);
-  const toRemove = [...nonoDivs, ...promoDivs];
-  
-  // filtering
-  toRemove.filter(div => div).forEach(div => div.style.display = "none");
+  const spans = Array.from(document.querySelectorAll('span'))
+    .filter(span => span.textContent && nonoWords.has(span.textContent.trim().toLowerCase()));
+  spans.forEach(span => removeRootElement(span));
+}
+
+function removeRootElement(elem) {
+  let curr = elem;
+  while (curr !== null && !nonoRootSelectorList.some(selector => curr.matches(selector))) {
+    curr = curr.parentElement;
+  }
+  if (curr) {
+    curr.parentElement.remove();
+  }
 }
 
 const observer = new MutationObserver(() => {
