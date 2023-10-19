9002f14db03b5abf9b0a1a54c18a58990b89ddb2
Brian Smith <briansmith@brians-mbp.lan>
2023-10-19 13:42:28 -0400

    catch nonowords in spans with child elements

--- firefox/contentScript.js
+++ firefox/contentScript.js
@@ -1,30 +1,37 @@
 const nonoWordList = [
-  'promoted',
-  'promoted by ',
-  'likes this',
-  'loves this',
-  'commented on this',
-  'finds this funny',
-  'celebrates this',
-  'supports this',
-  'finds this insightful',
+  "promoted",
+  "promoted by",
+  "likes this",
+  "loves this",
+  "commented on this",
+  "finds this funny",
+  "celebrates this",
+  "supports this",
+  "finds this insightful",
 ];
 const nonoWords = new Set(nonoWordList);
 
 const nonoRootSelectorList = [
-  'div.update-components-header',
-  'div.update-components-actor',
+  "div.update-components-header",
+  "div.update-components-actor",
 ];
 
 function filterUnwantedPosts() {
-  const spans = Array.from(document.querySelectorAll('span'))
-    .filter(span => span.textContent && nonoWords.has(span.textContent.trim().toLowerCase()));
-  spans.forEach(span => removeRootElement(span));
+  const spans = Array.from(document.querySelectorAll("span")).filter(
+    (span) =>
+      // span.childNodes[0].nodeValue represents the opening text of a span
+      span.childNodes[0].nodeValue &&
+      nonoWords.has(span.childNodes[0].nodeValue.trim().toLowerCase())
+  );
+  spans.forEach((span) => removeRootElement(span));
 }
 
 function removeRootElement(elem) {
   let curr = elem;
-  while (curr !== null && !nonoRootSelectorList.some(selector => curr.matches(selector))) {
+  while (
+    curr !== null &&
+    !nonoRootSelectorList.some((selector) => curr.matches(selector))
+  ) {
     curr = curr.parentElement;
   }
   if (curr) {
