279f5c44abb362c6e6a7b2d96b096a4cb8c44c1b
Brian Smith <briansmith@brians-mbp.lan>
2023-11-10 15:04:56 -0500

    fix error reading tab.url

--- chrome/background.js
+++ chrome/background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url.includes('linkedin')) {
+  if (changeInfo.status === 'complete' && tab.url?.includes('linkedin')) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
       files: ['contentScript.js']
