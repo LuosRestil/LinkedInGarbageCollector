73f84ce15180c07d90f8156aa977357c30023c0f
Luos Restil <luosrestil@luoss-mbp.lan>
2023-11-10 13:59:02 -0500

    remove lines left by removed cards, auto-scroll

--- firefox/contentScript.js
+++ firefox/contentScript.js
@@ -1,20 +1,44 @@
-const nonoWordList = [
+let loadButton;
+
+document.addEventListener("scroll", () => {
+  if (!loadButton) {
+    findLoadButton();
+    return;
+  }
+  const buttonPosition =
+    loadButton.getBoundingClientRect().top + window.scrollY;
+  const userScrollPosition = window.scrollY + window.innerHeight;
+  const delta = buttonPosition - userScrollPosition;
+  if (delta < 100) {
+    loadButton.click();
+    loadButton = findLoadButton();
+  }
+});
+
+function findLoadButton() {
+  loadButton = Array.from(document.querySelectorAll("button")).find((btn) =>
+    btn.textContent.includes("Show more feed updates")
+  );
+}
+
+const nonoWords = new Set([
   "promoted",
-  "promoted by",
-  "likes this",
-  "loves this",
-  "commented on this",
-  "finds this funny",
-  "celebrates this",
-  "supports this",
-  "finds this insightful",
-];
-const nonoWords = new Set(nonoWordList);
+  "likes",
+  "liked",
+  "loves",
+  "loved",
+  "commented",
+  "finds",
+  "celebrates",
+  "supports",
+]);
 
 const nonoRootSelectorList = [
   "div.update-components-header",
   "div.update-components-actor",
 ];
+
+const nonoRootParentSelector = "div.relative";
 
 function filterUnwantedPosts() {
   const spans = Array.from(document.querySelectorAll("span")).filter(
@@ -34,7 +58,11 @@ function removeRootElement(elem) {
   ) {
     curr = curr.parentElement;
   }
+
   if (curr) {
+    while (curr !== null && !curr.matches(nonoRootParentSelector)) {
+      curr = curr.parentElement;
+    }
     curr.parentElement.remove();
   }
 }
